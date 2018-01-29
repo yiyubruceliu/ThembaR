9290113f44f070a8426af73108fa6fede1a441a7
Yi-Yu Bruce Liu <yiyubruceliu@gmail.com>
2018-01-29 14:09:11 +0200

    initial structure. Contract creation

---
+++ contracts/Arrangement.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.16;
+
+contract Arrangement {
+    private address arranger;
+    private address arrangee;
+    private uint direction;
+    private bool signed;
+    mapping(uint => uint) private range;
+    //variables
+
+    //functions
+    public Arrangement(address _arranger,address arrangee, uint _direction, mapping(uint => uint) _range) {
+        //initialize the arrangement of the two accounts
+        //signed == false
+    }
+
+     SignArrangement(address signer) public returns (bool) {
+         //signed ==true
+    }
+
+     
+}
