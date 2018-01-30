869ccf120715a8c05c250d7ae9d9c16f678f1bd8
root <rendi619@gmail.com>
2018-01-30 14:18:50 +0200

    lad

--- test/thembaR.js
+++ test/thembaR.js
@@ -1,12 +1,13 @@
-var ThembaR = artifacts.require("./ThembaR.sol");
+const ThembaR = artifacts.require("./ThembaR.sol");
 
-contract('ThembaR', function(accounts) {
-    it("should put 0 ThembaR tokens in the first account", function() {
-        return ThembaR.deployed().then(function(instance) {
-          return instance.balanceOf.call({from: web3.eth.accounts[1], gas:3000000});
-        }).then(function(balance) {
-          assert.equal(balance.valueOf(), 0, "0 was in the first account");
-        });
-      });
-      
-});
+contract('ThembaR', function ([owner]) {
+  let thembaR
+  beforeEach('setup contract for each test', async function () {
+    thembaR = await ThembaR.new(owner, "ddf", "dsd")
+})
+
+it('has an owner', async function () {
+  console.log(owner)
+  assert.equal(await thembaR.getOwner(), owner)
+})
+})
