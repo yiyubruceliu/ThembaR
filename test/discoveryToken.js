a1c5843254a282aa64f55b47ebd87a42c7a0700b
Yi-Yu Bruce Liu <yiyubruceliu@gmail.com>
2018-01-30 08:12:00 +0200

    asd

--- test/discoveryToken.js
+++ test/discoveryToken.js
@@ -3,7 +3,7 @@ var discovery = artifacts.require("./MiniMeToken.sol");
 contract('MiniMeToken', function(accounts) {
     it("should put 0 MiniMeToken in the first account", function() {
         return discovery.deployed().then(function(instance) {
-          return instance.balanceOf.call(accounts[0]);
+          return instance.balanceOf.call({from: web3.eth.accounts[1], gas:3000000});
         }).then(function(balance) {
           assert.equal(balance.valueOf(), 0, "0 was in the first account");
         });
@@ -11,7 +11,7 @@ contract('MiniMeToken', function(accounts) {
       
     it("should put 1000 MiniMeTokens in the first account", function() {
         return discovery.deployed().then(function(instance) {
-            instance.generateTokens.call(accounts[0], 1000);
+            instance.generateTokens.call({from: web3.eth.accounts[1], gas:3000000} );
             return instance.balanceOf.call(instance);
         }).then(function(balance) {
             assert.equal(balance.valueOf(), 1000, "1000 was in the first account");
--- test/thembaR.js
+++ test/thembaR.js
@@ -3,7 +3,7 @@
 // contract('ThembaR', function(accounts) {
 //     it("should put 0 ThembaR tokens in the first account", function() {
 //         return ThembaR.deployed().then(function(instance) {
-//           return instance.balanceOf.call(accounts[0]);
+//           return instance.balanceOf.call({from: web3.eth.accounts[1], gas:3000000});
 //         }).then(function(balance) {
 //           assert.equal(balance.valueOf(), 0, "0 was in the first account");
 //         });
